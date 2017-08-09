ea7b5e6cf80dd39b76548ace1811d5560cfaa1a9
soundreactor <swisswiss@aol.com>
2017-08-09 16:27:54 +0200

    interaction design improvements

--- js/megatag.js
+++ js/megatag.js
@@ -9,113 +9,147 @@ var xmlhttp = {};
 var taglist = [];
 
 
-function alerta(a,b){
-	console.log(a);
-	console.log(b);
+function alerta(a, b) {
+  console.log(a);
+  console.log(b);
 }
 
 
-function AddWrapper(elem){
-	var itm = document.getElementById("megatag_template_wrap");
-	var cln = itm.cloneNode(true);
-	cln.removeAttribute('id');
-	var inptelm = cln.getElementsByTagName('input')[0];
-	elem.appendChild(cln);
-	$(inptelm).typeahead({
-	  hint: true,
-	  highlight: true,
-	  minLength: 1
-	},
-	{
-	  name: 'taglist',
-	  source: substringMatcher(taglist)
-	});
-	var cmnd = {"action":"get_tags","item":elem.getAttribute('data')};
-	SendCommand(cmnd,AddTags,elem);
+function AddWrapper(elem) {
+  var itm = document.getElementById("megatag_template_wrap");
+  var cln = itm.cloneNode(true);
+  cln.removeAttribute('id');
+  var inptelm = cln.getElementsByTagName('input')[0];
+  elem.appendChild(cln);
+  $(inptelm).typeahead({
+    hint: true,
+    highlight: true,
+    minLength: 1
+  }, {
+    name: 'taglist',
+    source: substringMatcher(taglist)
+  });
+  var cmnd = {
+    "action": "get_tags",
+    "item": elem.getAttribute('data')
+  };
+  SendCommand(cmnd, AddTags, elem);
 }
 
 
-function AddTags(obj,elem){
-	var ojson = JSON.parse(obj);
-	console.log(ojson.tag);
-	for (var i = 0; i < ojson.tag.length; i++) {
-		var mainelem = elem.getElementsByClassName("megatag-main")[0];
-		var itm = document.getElementById("megatag_template_tag");
-		var cln = itm.cloneNode(true);
-		cln.removeAttribute('id');
-		cln.childNodes[0].nodeValue = ojson['tag'][i]+" ";
-		cln.getElementsByTagName('span')[0].addEventListener("click", function(theVar){return function(){DeleteTag(theVar)};}(cln),false);
-		mainelem.insertBefore(cln, mainelem.childNodes[0]);
-	}
+function AddTags(obj, elem) {
+  var ojson = JSON.parse(obj);
+  //console.log(ojson.tag);
+  for (var i = 0; i < ojson.tag.length; i++) {
+    var mainelem = elem.getElementsByClassName("megatag-main")[0];
+    var itm = document.getElementById("megatag_template_tag");
+    var cln = itm.cloneNode(true);
+    cln.removeAttribute('id');
+    cln.childNodes[0].nodeValue = ojson['tag'][i] + " ";
+    cln.getElementsByTagName('span')[0].addEventListener("click", function(theVar) {
+      return function() {
+        DeleteTag(theVar)
+      };
+    }(cln), false);
+    mainelem.insertBefore(cln, mainelem.childNodes[mainelem.childNodes.length - 1]);
+  }
 }
 
-function SendTag(refr){
-	var itemname = refr.parentNode.parentNode.parentNode.getAttribute('data');
-	var strobj = '{"tag":["'+refr.value+'"]}';
-	AddTags(strobj,refr.parentNode.parentNode.parentNode);
-	var objcmd = {"action":"add_tag","tag":refr.value,"item":itemname};
-	SendCommand(objcmd,alerta,refr);
-	refr.value = "";
+function SendTag(refr) {
+  var itemname = refr.parentNode.parentNode.parentNode.getAttribute('data');
+  var strobj = '{"tag":["' + refr.value + '"]}';
+  AddTags(strobj, refr.parentNode.parentNode.parentNode);
+  var objcmd = {
+    "action": "add_tag",
+    "tag": refr.value,
+    "item": itemname
+  };
+  SendCommand(objcmd, alerta, refr);
+  refr.value = "";
+	//clean typeahead
+	$(refr).typeahead('val', '');
 }
 
-function DeleteTag(elm){
-	var tagnme = elm.childNodes[0].nodeValue;
-	tagnme.trim();    
-	var itemname = elm.parentNode.parentNode.getAttribute('data');
-	var cmdd = {"action":"delete_tag","tag":tagnme,"item":itemname};
-	SendCommand(cmdd,alerta,elm);
-	elm.remove();
+function DeleteTag(elm) {
+  var tagnme = elm.childNodes[0].nodeValue;
+  tagnme.trim();
+  var itemname = elm.parentNode.parentNode.getAttribute('data');
+  var cmdd = {
+    "action": "delete_tag",
+    "tag": tagnme,
+    "item": itemname
+  };
+  SendCommand(cmdd, alerta, elm);
+  elm.remove();
 }
 
-function SendCommand(obj,callback,elemnd){
-	dbParam = JSON.stringify(obj);
-	var tmprndmname = makeid();
-	xmlhttp[tmprndmname] = new XMLHttpRequest();
-	xmlhttp[tmprndmname].onreadystatechange = function()
-    {
-        if (xmlhttp[tmprndmname].readyState == 4 && xmlhttp[tmprndmname].status == 200)
-        {
-            callback(xmlhttp[tmprndmname].responseText,elemnd); // Another callback here
+function SendCommand(obj, callback, elemnd) {
+  dbParam = JSON.stringify(obj);
+  var tmprndmname = makeid();
+  xmlhttp[tmprndmname] = new XMLHttpRequest();
+  xmlhttp[tmprndmname].onreadystatechange = function() {
+    if (xmlhttp[tmprndmname].readyState == 4 && xmlhttp[tmprndmname].status == 200) {
+      callback(xmlhttp[tmprndmname].responseText, elemnd); // Another callback here
+    }
+  };
+  xmlhttp[tmprndmname].open("GET", server + "?x=" + dbParam, true);
+  xmlhttp[tmprndmname].send();
+}
+
+function GetAllTags(obj, elmt) {
+  var nutaglistobj = JSON.parse(obj);
+  taglist = nutaglistobj['tag'];
+  taglist = taglist.filter(function(item, index, inputArray) {
+    return inputArray.indexOf(item) == index;
+  });
+
+  aftergetlist();
+}
+
+function SearchItems(bangz) {
+  var cmnd = {
+    "action": "get_items",
+    "tag": bangz
+  };
+  SendCommand(cmnd, alerta, window);
+}
+
+//DO AFTER DOC IS LOADED
+// document.addEventListener("DOMContentLoaded", function(event) {
+function aftergetlist() {
+  var classname2 = document.getElementsByClassName("megatag-s");
+  for (var i = 0; i < classname2.length; i++) {
+    //console.log("wtf");
+    AddWrapper(classname2[i]);
+  }
+  // ADD TAG FROM INPUT
+  var classname = document.getElementsByClassName("megatag-input tt-input");
+  for (var i = 0; i < classname.length; i++) {
+    classname[i]
+      .addEventListener("keyup", function(event) {
+        event.preventDefault();
+        if (event.keyCode == 13) {
+          SendTag(this);
         }
-    }; 
-	xmlhttp[tmprndmname].open("GET", server+"?x=" + dbParam, true);
-	xmlhttp[tmprndmname].send();
-}
+      });
+  }
 
-function GetAllTags(obj,elmt){
-	var nutaglistobj = JSON.parse(obj);
-	taglist = nutaglistobj['tag'];
-	taglist = taglist.filter( function( item, index, inputArray ) {
-           return inputArray.indexOf(item) == index;
-    });
-
-	aftergetlist();
-}
-
-function SearchItems(bangz){
-	var cmnd = {"action":"get_items","tag":bangz};
-	SendCommand(cmnd,alerta,window);
-}
-
-//DO AFTER DOC IS LOADED 
-// document.addEventListener("DOMContentLoaded", function(event) { 
-function aftergetlist(){
-	var classname2 = document.getElementsByClassName("megatag-s");
-	for (var i = 0; i < classname2.length; i++) {
-		//console.log("wtf");
-		AddWrapper(classname2[i]);
-	}
-	  // ADD TAG FROM INPUT
-	var classname = document.getElementsByClassName("megatag-input tt-input");
-	for (var i = 0; i < classname.length; i++) {
-		classname[i]
-			.addEventListener("keyup", function(event) {
-			event.preventDefault();
-			if (event.keyCode == 13) {
-				SendTag(this);
-			}
-		});
-	} 
+  // ADD SEARCH BAR EVENTS
+  var classname = document.getElementsByClassName("megatag-item-search");
+  for (var i = 0; i < classname.length; i++) {
+    classname[i]
+      .addEventListener("keyup", function(event) {
+        event.preventDefault();
+        if (event.keyCode == 13) {
+          console.log(window.location.href);
+          var urll = this.getAttribute('data');
+          var clbkloc = window.location = location.protocol + '//' + location.host + location.pathname;
+          var cmdd = '?x={"action":"get_items","tag":"' + this.value + '","callback":"' + clbkloc + '"}';
+          console.log(urll + cmdd);
+          window.location = urll + cmdd;
+        }
+      });
+  }
 }
 //});
 
@@ -125,15 +159,17 @@ function aftergetlist(){
 //START
 var div = document.createElement("div");
 
-div.innerHTML = '<span id="megatag_template_tag" class="megatag-tag label label-info">TEMP <span class="megatag-remove glyphicon glyphicon-minus"></span></span>'+
-'<div id="megatag_template_wrap" onclick="this.getElementsByTagName(\'input\')[1].focus()" class="megatag-main">'+
-'<input  class="megatag-input" type="text" ></div>';
+div.innerHTML = '<span id="megatag_template_tag" class="megatag-tag label label-info">TEMP <span class="megatag-remove glyphicon glyphicon-minus"></span></span>' +
+  '<div id="megatag_template_wrap" onclick="this.getElementsByTagName(\'input\')[1].focus()" class="megatag-main">' +
+  '<input  class="megatag-input" type="text" ></div>';
 
 document.body.appendChild(div);
 
 
-var cmnd = {"action":"get_all_tags"};
-SendCommand(cmnd,GetAllTags,window);
+var cmnd = {
+  "action": "get_all_tags"
+};
+SendCommand(cmnd, GetAllTags, window);
 
 
 
@@ -142,7 +178,9 @@ SendCommand(cmnd,GetAllTags,window);
 function makeid() {
   var text = "";
   var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  for (var i = 0; i < 10; i++){text += possible.charAt(Math.floor(Math.random() * possible.length))};
+  for (var i = 0; i < 10; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length))
+  };
   return text;
 }
 
@@ -164,21 +202,21 @@ taglist = ['Alabama', 'Alaska'];
 
 
 
- 
+
 
 // function SendCommand(obj, callback, elemnd){
 
-	// dbParam = JSON.stringify(obj);
-	// xmlhttp = new XMLHttpRequest();
-	// xmlhttp.onreadystatechange = function()
-    // {
-        // if (xmlhttp.readyState == 4 && xmlhttp.status == 200)
-        // {
-            // callback(xmlhttp.responseText,elemnd); // Another callback here
-        // }
-    // }; 
-	// xmlhttp.open("GET", server+"?x=" + dbParam, true);
-	// xmlhttp.send();
+// dbParam = JSON.stringify(obj);
+// xmlhttp = new XMLHttpRequest();
+// xmlhttp.onreadystatechange = function()
+// {
+// if (xmlhttp.readyState == 4 && xmlhttp.status == 200)
+// {
+// callback(xmlhttp.responseText,elemnd); // Another callback here
+// }
+// };
+// xmlhttp.open("GET", server+"?x=" + dbParam, true);
+// xmlhttp.send();
 
 // }
 //focus input
@@ -186,15 +224,15 @@ taglist = ['Alabama', 'Alaska'];
 // var classname = document.getElementsByClassName("megatag-main");
 
 // var focus_input = function() {
-    // var inputobj = this.getElementsByTagName('input')[0];
-    // console.log(inputobj);
-	// inputobj.focus();
+// var inputobj = this.getElementsByTagName('input')[0];
+// console.log(inputobj);
+// inputobj.focus();
 // };
 
 // for (var i = 0; i < classname.length; i++) {
-    // classname[i].addEventListener('click', focus_input, false);
+// classname[i].addEventListener('click', focus_input, false);
 // }
- // });
+// });
 /*
 obj = {"action":"delete_tag","item":"ffffff","tag":"Beton"};
 
--- megatag.php
+++ megatag.php
@@ -1,175 +1,177 @@
 <?php
 // MIT license
-
 $conn = new mysqli("localhost", "root", "", "dudler_project_tags");
-
-
 // only edit below here if you have IQ > 247
 header("Content-Type: application/json; charset=UTF-8");
 $obj = json_decode($_GET["x"], false);
-
 // GET ITEMS
-if($obj->action == "get_items"){
-	$result = $conn->query("SELECT
+
+if ($obj->action == "get_items")
+{
+    $result = $conn->query("SELECT
    tbl_items.value
 FROM
    tbl_item_tag_ref
     JOIN tbl_tags ON tbl_item_tag_ref.tag_id = tbl_tags.id
     JOIN tbl_items ON tbl_item_tag_ref.item_id = tbl_items.id
 WHERE
-    tbl_tags.tag = '".$obj->tag."'");
+    tbl_tags.tag = '" . $obj->tag . "'");
+    $outp = array();
+    $outp = $result->fetch_all(MYSQLI_ASSOC);
+    $nuout = array();
 
-	$outp = array();
-	$outp = $result->fetch_all(MYSQLI_ASSOC);
-	
-	$nuout = array();
+    foreach ($outp as $rey)
+    {
+        $itm = $rey['value'];
+        $tmpts = GetTags($conn, $itm);
+        $objecta = ['item' => $rey['value'], 'tag' => $tmpts['tag']];
+        array_push($nuout, $objecta);
+    }
+    //print_r($nuout);
+    $jsonout = json_encode($nuout);
 
-	foreach ($outp as $rey) {
-		$itm = $rey['value'];
-		
-		$tmpts = GetTags($conn,$itm);
-		
-		$objecta = ['item' => $rey['value'], 'tag' => $tmpts['tag'] ];
-		array_push($nuout, $objecta);
-	}
-	
-	//print_r($nuout);
-	$jsonout =  json_encode($nuout);
-	
-	if(isset($obj->callback)){
-		header("Location: ".$obj->callback."?x=".$jsonout); /* Redirect browser */
-		exit();
-	}else{
-		echo $jsonout;
-	}
-	
+    if (isset($obj->callback))
+    {
+        header("Location: " . $obj->callback . "?x=" . $jsonout); /* Redirect browser */
+        exit();
+    }
+    else
+    {
+        echo $jsonout;
+    }
 }
 // GET TAGS
-if($obj->action == "get_tags"){
 
-	$tmpo = GetTags($conn,$obj->item);
-	echo json_encode($tmpo);
+if ($obj->action == "get_tags")
+{
+    $tmpo = GetTags($conn, $obj->item);
+    echo json_encode($tmpo);
 }
 // GET ALL ITEMS
-if($obj->action == "get_all_items"){
-	$result = $conn->query("SELECT
+
+if ($obj->action == "get_all_items")
+{
+    $result = $conn->query("SELECT
    tbl_items.value FROM
    tbl_items");
+    $outp = array();
+    $outp = $result->fetch_all(MYSQLI_ASSOC);
+    $nuout = array();
 
-	$outp = array();
-	$outp = $result->fetch_all(MYSQLI_ASSOC);
-	
-	$nuout = array();
-
-	foreach ($outp as $rey) {
-		$itm = $rey['value'];
-		
-		$tmpts = GetTags($conn,$itm);
-		
-		$objecta = ['item' => $rey['value'], 'tag' => $tmpts['tag'] ];
-		array_push($nuout, $objecta);
-	}
-	
-	//print_r($nuout);
-	echo json_encode($nuout);
-
+    foreach ($outp as $rey)
+    {
+        $itm = $rey['value'];
+        $tmpts = GetTags($conn, $itm);
+        $objecta = ['item' => $rey['value'], 'tag' => $tmpts['tag']];
+        array_push($nuout, $objecta);
+    }
+    //print_r($nuout);
+    echo json_encode($nuout);
 }
 // GET ALL TAGS
-if($obj->action == "get_all_tags"){
-	$result = $conn->query("SELECT
+
+if ($obj->action == "get_all_tags")
+{
+    $result = $conn->query("SELECT
    tbl_tags.tag FROM
    tbl_tags");
+    $outp = array();
+    $outp = $result->fetch_all(MYSQLI_ASSOC);
+    $objecta = ['tag' => array() ];
+    $nunu = array();
 
-	$outp = array();
-	$outp = $result->fetch_all(MYSQLI_ASSOC);
-	
-	$objecta = ['tag' => array() ];
-  
-	$nunu =  array();
-	foreach ($outp as $rey) {
-		
-		array_push($objecta['tag'], $rey['tag']);
-	}
-	//return $objecta;
-	echo json_encode($objecta);
-
+    foreach ($outp as $rey)
+    {
+        array_push($objecta['tag'], $rey['tag']);
+    }
+    //return $objecta;
+    echo json_encode($objecta);
 }
 // ADD TAG
-if($obj->action == "add_tag"){
-	// get or set item
-	$result = $conn->query("SELECT id FROM tbl_items WHERE tbl_items.value = '".$obj->item."'");
-	if($result->num_rows === 0){
-		$result = $conn->query("INSERT INTO tbl_items (value) VALUES('".$obj->item."')");
-		$result = $conn->query("SELECT id FROM tbl_items WHERE tbl_items.value = '".$obj->item."'");
-	}
-	$outp = array();
-	$outp = $result->fetch_all(MYSQLI_ASSOC);
-	$item_id = $outp[0]["id"];
-	// get or set tag
-	$result = $conn->query("SELECT id FROM tbl_tags WHERE tbl_tags.tag = '".$obj->tag."'");
-	if($result->num_rows === 0){
-		$result = $conn->query("INSERT INTO tbl_tags (tag) VALUES('".$obj->tag."')");
-		$result = $conn->query("SELECT id FROM tbl_tags WHERE tbl_tags.tag = '".$obj->tag."'");
-	}
-	$outp = array();
-	$outp = $result->fetch_all(MYSQLI_ASSOC);
-	$tag_id = $outp[0]["id"];
-	
-	$result = $conn->query("SELECT * FROM tbl_item_tag_ref WHERE tbl_item_tag_ref.tag_id = '".$tag_id."' AND tbl_item_tag_ref.item_id = '".$item_id."'");
-	if($result->num_rows === 0){
-		$result = $conn->query("INSERT INTO tbl_item_tag_ref (tag_id,item_id) VALUES('".$tag_id."','".$item_id."')");
-		$result = $conn->query("SELECT * FROM tbl_item_tag_ref WHERE tbl_item_tag_ref.tag_id = '".$tag_id."' AND tbl_item_tag_ref.item_id = '".$item_id."'");
-	}
-	if($result->num_rows !== 0){
-		echo "{\"status\":\"OK\"}";
-	}
-}
-if($obj->action == "delete_tag"){
-	$result = $conn->query("SELECT id FROM tbl_items WHERE tbl_items.value = '".$obj->item."'");
-	if($result->num_rows === 0){
-		exit("{\"status\":\"ERROR no item\"}");
-	}
-	$outp = array();
-	$outp = $result->fetch_all(MYSQLI_ASSOC);
-	$item_id = $outp[0]["id"];
-	// get or set tag
-	$result = $conn->query("SELECT id FROM tbl_tags WHERE tbl_tags.tag = '".$obj->tag."'");
-	if($result->num_rows === 0){
-		exit("{\"status\":\"ERROR no tag\"}");
-	}
-	$outp = array();
-	$outp = $result->fetch_all(MYSQLI_ASSOC);
-	$tag_id = $outp[0]["id"];
-	
-	$result = $conn->query("DELETE FROM tbl_item_tag_ref WHERE tbl_item_tag_ref.tag_id = '".$tag_id."' AND tbl_item_tag_ref.item_id = '".$item_id."'");
-	echo "{\"status\":\"OK\"}";
+
+if ($obj->action == "add_tag")
+{
+    // get or set item
+    $result = $conn->query("SELECT id FROM tbl_items WHERE tbl_items.value = '" . $obj->item . "'");
+
+    if ($result->num_rows === 0)
+    {
+        $result = $conn->query("INSERT INTO tbl_items (value) VALUES('" . $obj->item . "')");
+        $result = $conn->query("SELECT id FROM tbl_items WHERE tbl_items.value = '" . $obj->item . "'");
+    }
+    $outp = array();
+    $outp = $result->fetch_all(MYSQLI_ASSOC);
+    $item_id = $outp[0]["id"];
+    // get or set tag
+    $result = $conn->query("SELECT id FROM tbl_tags WHERE tbl_tags.tag = '" . $obj->tag . "'");
+
+    if ($result->num_rows === 0)
+    {
+        $result = $conn->query("INSERT INTO tbl_tags (tag) VALUES('" . $obj->tag . "')");
+        $result = $conn->query("SELECT id FROM tbl_tags WHERE tbl_tags.tag = '" . $obj->tag . "'");
+    }
+    $outp = array();
+    $outp = $result->fetch_all(MYSQLI_ASSOC);
+    $tag_id = $outp[0]["id"];
+    $result = $conn->query("SELECT * FROM tbl_item_tag_ref WHERE tbl_item_tag_ref.tag_id = '" . $tag_id . "' AND tbl_item_tag_ref.item_id = '" . $item_id . "'");
+
+    if ($result->num_rows === 0)
+    {
+        $result = $conn->query("INSERT INTO tbl_item_tag_ref (tag_id,item_id) VALUES('" . $tag_id . "','" . $item_id . "')");
+        $result = $conn->query("SELECT * FROM tbl_item_tag_ref WHERE tbl_item_tag_ref.tag_id = '" . $tag_id . "' AND tbl_item_tag_ref.item_id = '" . $item_id . "'");
+    }
+
+    if ($result->num_rows !== 0)
+    {
+        echo "{\"status\":\"OK\"}";
+    }
 }
 
-function GetTags($conn,$item){
-		$result = $conn->query("SELECT
+if ($obj->action == "delete_tag")
+{
+    $result = $conn->query("SELECT id FROM tbl_items WHERE tbl_items.value = '" . $obj->item . "'");
+
+    if ($result->num_rows === 0)
+    {
+        exit("{\"status\":\"ERROR no item\"}");
+    }
+    $outp = array();
+    $outp = $result->fetch_all(MYSQLI_ASSOC);
+    $item_id = $outp[0]["id"];
+    // get or set tag
+    $result = $conn->query("SELECT id FROM tbl_tags WHERE tbl_tags.tag = '" . $obj->tag . "'");
+
+    if ($result->num_rows === 0)
+    {
+        exit("{\"status\":\"ERROR no tag\"}");
+    }
+    $outp = array();
+    $outp = $result->fetch_all(MYSQLI_ASSOC);
+    $tag_id = $outp[0]["id"];
+    $result = $conn->query("DELETE FROM tbl_item_tag_ref WHERE tbl_item_tag_ref.tag_id = '" . $tag_id . "' AND tbl_item_tag_ref.item_id = '" . $item_id . "'");
+    echo "{\"status\":\"OK\"}";
+}
+function GetTags($conn, $item)
+{
+    $result = $conn->query("SELECT
    tbl_tags.tag
 FROM
    tbl_item_tag_ref
     JOIN tbl_tags ON tbl_item_tag_ref.tag_id = tbl_tags.id
     JOIN tbl_items ON tbl_item_tag_ref.item_id = tbl_items.id
 WHERE
-    tbl_items.value = '".$item."'");
+    tbl_items.value = '" . $item . "'");
+    $outp = array();
+    $outp = $result->fetch_all(MYSQLI_ASSOC);
+    $objecta = ['tag' => array() ];
+    $nunu = array();
 
-	$outp = array();
-	$outp = $result->fetch_all(MYSQLI_ASSOC);
-
-	$objecta = ['tag' => array() ];
-  
-	$nunu =  array();
-	foreach ($outp as $rey) {
-		
-		array_push($objecta['tag'], $rey['tag']);
-	}
-	return $objecta;
+    foreach ($outp as $rey)
+    {
+        array_push($objecta['tag'], $rey['tag']);
+    }
+    return $objecta;
 }
-
-
-
 /*
 query("SELECT
    tbl_items.value, tbl_tags.tag
@@ -178,9 +180,7 @@ FROM
     JOIN tbl_tags ON tbl_item_tag_ref.tag_id = tbl_tags.id
     JOIN tbl_items ON tbl_item_tag_ref.item_id = tbl_items.id");
 */
-
 //$result = $conn->query("SELECT name FROM ".$obj->table." LIMIT ".$obj->limit);
-
 //if($obj->)
 
 ?>
